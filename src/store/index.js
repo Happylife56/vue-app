383a663d4d71cec1ebb0948f585df6d3b3435e62
liukai <3001397597@qq.com>
2019-10-11 14:53:06 +0800

    优化store模块引入方式

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,25 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 
-import collect from './modules/collect'
-import common from './modules/common'
+
+/**
+ * 读取./modules下的所有js文件并注册模块
+ */
+
+const requireModule = require.context('./modules', true, /\.js$/)
+const module = {}
+requireModule.keys().forEach(key => {
+  const moduleName = key.replace(/(\.\/|.*\/|\.js)/g, '')
+  module[moduleName] = {
+    ...requireModule(key).default
+  }
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    collect, // 收银
-    common // 公用
+    ...module
   },
   plugins: [
     createPersistedState({
